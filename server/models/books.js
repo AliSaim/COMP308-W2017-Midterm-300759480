d5614075d639ecb269014b65d34526ee12a0f3fb
AliSaim <Ali@Alis-MacBook-Air.local>
2017-02-25 15:20:42 -0500

    Added internal internal documentaion to js files

--- server/models/books.js
+++ server/models/books.js
@@ -1,3 +1,10 @@
+//-- INTERNAL DOCUMENTATION -->
+//-- File Name: books.js -->
+//-- Student Name: Ali Saim -->
+//-- Student ID: 300759480 -->
+//-- Web App Name: My Favourite Books -->
+//-- Date: Feb 25th 2017 -->
+
 let mongoose = require('mongoose');
 
 // create a model class
